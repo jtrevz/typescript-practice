3ae617a292bad48a18ace73f27c46790ec835ba1
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-23 23:19:39 -0500

    types, arrays, enums

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-let age: number = 20;
+//TYPES
+// let sales = 123_456_789;
+// let course = "Typescript";
+// let is_published = true;
+// let level;
 
-if (age < 50)
-    age += 10;
-console.log(age)
+// function render(document) {
+//   console.log(document);
+// }
+
+//ARRAYS
+// let numbers:number[] = [];
+// numbers.forEach(n => n.toExponential)
+
+//tuple?
+// let user:[number, string] = [1, "Mosh"]
+// user.push(1);
+
+//ENUMS: list of related constants, pascal naming convention?
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// const enum Size {Small = 1, Medium, Large};
+
+// let mySize: Size = Size.Small;
+// console.log(mySize);
