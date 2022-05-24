cbcf863ae2a9375c4d5312b73674c30ca0b28b65
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-24 11:40:34 -0500

    objects(long way)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-//TYPES
+//TYPES------------------------------
 // let sales = 123_456_789;
 // let course = "Typescript";
 // let is_published = true;
@@ -8,11 +8,11 @@
 //   console.log(document);
 // }
 
-//ARRAYS
+//ARRAYS-----------------------------
 // let numbers:number[] = [];
 // numbers.forEach(n => n.toExponential)
 
-//tuple?
+//TUPLES?----------------------------
 // let user:[number, string] = [1, "Mosh"]
 // user.push(1);
 
@@ -26,20 +26,53 @@
 // let mySize: Size = Size.Small;
 // console.log(mySize);
 
-//Functions
+//FUNCTIONS------------------------------
 
 // setting parameters to accept type of value but can also set function to return type of value or void if no return
 
 // JS returns undefined if no value is returned => not a number
 // param optional by addign question mark, you could give param default value
 
-function calculateTax(income: number, taxYear = 2022): number {
-  if (taxYear < 2022) return income * 1.2;
-  return income * 1.3;
-}
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 2022) return income * 1.2;
+//   return income * 1.3;
+// }
 
-calculateTax(10_000)
+// calculateTax(10_000)
 
-//notes: 
+//notes:
 // -properly anotate fxns w. params/return types
 // -Enable: "noUnusedLocals", "noUnusedParameters", "noImplicitReturns"
+
+// OBJECTS-------------------------------
+
+// let employee: {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// } = { id: 1, name: "Jenny", retire: (date: Date) => console.log(date)};
+
+//readonly modifier: typescript compiler prevents us from accidentally modifying value of property
+
+//issues: if initializing another employee, then you will have to anotate a completely new object, verbose and redundant
+//new employee might need to have different properties available to it, different shape
+//hard to read
+
+//ADVANCED TYPES ------------------------
+
+//Type Alias
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Jenny",
+  retire: (date: Date) => console.log(date),
+};
+
+//Union Types
+
